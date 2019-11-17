5ea5139f07acb53c952d09bd43c59878cfd9309f
Daniel <danielac@protonmail.com>
2019-11-17 12:04:06 -0700

    fix: buttons can be clicked more than once

--- app.js
+++ app.js
@@ -1,3 +1,16 @@
+/* TODO
+Turn all 4 upgrades into two dictionaries?
+
+Make methods collectAutoUpgrades & automaticUpgrade ?
+
+Make buy buttons disable = false once sufficient funds are reached
+
+Make pickaxes multiply click yield
+
+Make carts multiply click yield
+*/
+
+
 // data
 
 let totalGold = 0
@@ -50,90 +63,121 @@ function updateGoldDis() {
   Gold Grams: ${totalGold}`
 }
 
-// buying functions. after completion, make buy_() functions dynamic, & fire 2 functions per buy click. One will update data, & the other will re-render the page
-function buyPickaxe() {
-  if (totalGold < pickaxeOb.price) {
-    pickaxeDis.setAttribute("disabled", "true")
-  } else {
-    pickaxeDis.setAttribute("disabled", "false")
-
-    pickaxeOb.quantity++
-    document.querySelector("#pickaxe-count").innerHTML = `Pickaxes: ${pickaxeOb.quantity}`
-
-    totalGold -= pickaxeOb.price
-    document.querySelector("#gold-grams").innerHTML = `Gold Grams: ${totalGold}`
-
-    pickaxeOb.price *= 1.1
-    document.querySelector("#pickaxe-price").innerHTML = `Gold Grams: ${pickaxeOb.price}`
-  }
-}
-
-function buyCart() {
-  if (totalGold < cartOb.price) {
-    cartDis.setAttribute("disabled", "true")
-  } else {
-    cartDis.setAttribute("disabled", "false")
-
-    cartOb.quantity++
-    document.querySelector("#cart-count").innerHTML = `Carts: ${cartOb.quantity}`
-
-    totalGold -= cartOb.price
-    document.querySelector("#gold-grams").innerHTML = `Gold Grams: ${totalGold}`
-
-    cartOb.price *= 1.1
-    document.querySelector("#cart-price").innerHTML = `Gold Grams: ${cartOb.price}`
-  }
-}
-
-function buyMiner() {
-  if (totalGold < minerOb.price) {
-    minerDis.setAttribute("disabled", "true")
-  } else {
-    minerDis.setAttribute("disabled", "false")
-
-    minerOb.quantity++
-    document.querySelector("#miner-count").innerHTML = `Miners: ${minerOb.quantity}`
-
-    totalGold -= minerOb.price
-    document.querySelector("#gold-grams").innerHTML = `Gold Grams: ${totalGold}`
-
-    minerOb.price *= 1.1
-    document.querySelector("#miner-price").innerHTML = `Gold Grams: ${minerOb.price}`
-  }
-}
-
-function buyShaft() {
-  if (totalGold < pickaxeOb.price) {
-    shaftDis.setAttribute("disabled", "true")
-  } else {
-    shaftDis.setAttribute("disabled", "false")
-
-    shaftOb.quantity++
-    document.querySelector("#shaft-count").innerHTML = `Mine Shafts: ${shaftOb.quantity}`
-
-    totalGold -= shaftOb.price
-    document.querySelector("#gold-grams").innerHTML = `Gold Grams: ${totalGold}`
-
-    shaftOb.price *= 1.1
-    document.querySelector("#shaft-price").innerHTML = `Gold Grams: ${shaftOb.price}`
-  }
-}
-
 // auto functions
 // FIX: effectively increment, but don't draw to page every 3 seconds
+
 function autoMiner() {
   if (minerOb.quantity > 0) {
-    setInterval(function () {
-      mineMulti(minerOb.quantity * minerOb.multiplier)
-    }, 3000)
-    document.querySelector("#gold-grams").innerHTML = `Gold Grams: ${totalGold}`
+    setInterval(function () { mineMulti(minerOb.quantity * minerOb.multiplier) }, 1000)
   }
 }
 
 function autoShaft() {
   if (shaftOb.quantity > 0) {
-    setInterval(function () {
-      mineMulti(shaftOb.quantity * shaftOb.multiplier)
-    }, 3000)
+    setInterval(function () { mineMulti(shaftOb.quantity * shaftOb.multiplier) }, 1000)
   }
+}
+
+function autoDrawGold() {
+  setInterval(function () { document.querySelector("#gold-grams").innerHTML = `Gold Grams: ${totalGold}` }, 1000)
+}
+
+// buying functions. after completion, make buy_() functions dynamic, & fire 2 functions per buy click. One will update data, & the other will re-render the page
+
+// butten enabling functions
+function enablePickaxe() {
+  if (totalGold > pickaxeOb.price) {
+    pickaxeDis.setAttribute("disabled", "false")
+  }
+}
+
+function enableCart() {
+  if (totalGold > cartOb.price) {
+    cartDis.setAttribute("disabled", "false")
+  }
+}
+
+function enableMiner() {
+  if (totalGold > minerOb.price) {
+    minerDis.setAttribute("disabled", "false")
+  }
+}
+
+function enableShaft() {
+  if (totalGold > shaftOb.price) {
+    shaftDis.setAttribute("disabled", "false")
+  }
+}
+
+// buy button functions
+function buyPickaxe() {
+  if (totalGold < pickaxeOb.price) {
+    pickaxeDis.setAttribute("disabled", "true")
+  }
+  // else {
+  //   pickaxeDis.setAttribute("disabled", "false")
+  // }
+
+  pickaxeOb.quantity++
+  document.querySelector("#pickaxe-count").innerHTML = `Pickaxes: ${pickaxeOb.quantity}`
+
+  totalGold -= pickaxeOb.price
+  document.querySelector("#gold-grams").innerHTML = `Gold Grams: ${totalGold}`
+
+  pickaxeOb.price *= 1.1
+  document.querySelector("#pickaxe-price").innerHTML = `Gold Grams: ${pickaxeOb.price}`
+}
+
+function buyCart() {
+  if (totalGold < cartOb.price) {
+    cartDis.setAttribute("disabled", "true")
+  }
+  // else {
+  //   cartDis.setAttribute("disabled", "false")
+  // }
+
+  cartOb.quantity++
+  document.querySelector("#cart-count").innerHTML = `Carts: ${cartOb.quantity}`
+
+  totalGold -= cartOb.price
+  document.querySelector("#gold-grams").innerHTML = `Gold Grams: ${totalGold}`
+
+  cartOb.price *= 1.1
+  document.querySelector("#cart-price").innerHTML = `Gold Grams: ${cartOb.price}`
+}
+
+function buyMiner() {
+  if (totalGold < minerOb.price) {
+    minerDis.setAttribute("disabled", "true")
+  }
+  // else {
+  //   minerDis.setAttribute("disabled", "false")
+  // }
+
+  minerOb.quantity++
+  document.querySelector("#miner-count").innerHTML = `Miners: ${minerOb.quantity}`
+
+  totalGold -= minerOb.price
+  document.querySelector("#gold-grams").innerHTML = `Gold Grams: ${totalGold}`
+
+  minerOb.price *= 1.1
+  document.querySelector("#miner-price").innerHTML = `Gold Grams: ${minerOb.price}`
+}
+
+function buyShaft() {
+  if (totalGold < pickaxeOb.price) {
+    shaftDis.setAttribute("disabled", "true")
+  }
+  // else {
+  //   shaftDis.setAttribute("disabled", "false")
+  // }
+
+  shaftOb.quantity++
+  document.querySelector("#shaft-count").innerHTML = `Mine Shafts: ${shaftOb.quantity}`
+
+  totalGold -= shaftOb.price
+  document.querySelector("#gold-grams").innerHTML = `Gold Grams: ${totalGold}`
+
+  shaftOb.price *= 1.1
+  document.querySelector("#shaft-price").innerHTML = `Gold Grams: ${shaftOb.price}`
 }
